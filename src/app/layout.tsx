933008a58e6851de2bbfd8d7d784b7927607d25e
Solahin Sourjah <bcs.sourjah@gmail.com>
2025-07-10 08:05:52 +0000

    Error: ./src/app/layout.tsx:16:6 Parsing ecmascript source code failed 

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" style={{colorScheme: "dark"}}>
+    <html lang="en" className="dark">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"></link>
       </head>
       <body className="font-body antialiased">
